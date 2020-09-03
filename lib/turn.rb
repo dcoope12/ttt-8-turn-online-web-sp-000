3266ec49212967c9d03727b2e66006f48f89937a
Devin Cooper <dcoope12@asu.edu>
2020-09-03 22:46:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -47,7 +47,7 @@ end
 #Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
   def turn(board)
     puts "Please enter 1-9:"
-    number  = gets.strip
+    number  = gets.chomp
     index = input_to_index(number)
         if valid_move?(board, index) == true
       move(board, index)

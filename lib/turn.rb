8d28603f9d3f4b927f094c2d5e9221925263bee8
Devin Cooper <dcoope12@asu.edu>
2020-09-03 22:10:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,5 +1,13 @@
 def display_board(board)
-puts board
+  def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  
+  end
 end
 
 def position_taken?(board, index)
@@ -20,4 +28,8 @@ end
 
 def move(board, index, token = "X")
  board[index] = token
+end
+
+def turn
+
 end

79e0c5cd4f8f8f3cd3b02dca60520e5f784c96d7
Devin Cooper <dcoope12@asu.edu>
2020-09-03 22:41:16 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,7 +7,13 @@
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
   end
-#convert user's placeon board to index integer using input_to_index
+#convert user's place on board to index integer using input_to_index
+def input_to_index(input)  
+  index = input.to_i
+  input -= 1
+  return index
+end 
+end
 
 def valid_move?(board,index)
   def position_taken?(board, index)
@@ -42,4 +48,15 @@ end
 #Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
   def turn(board)
     puts "Please enter 1-9:"
+    number  = gets.strip
+    board[index] = number
+        if number is valid
+      move(board, index, character)
+    else
+  get input
+  convert input to index
+  if index is valid
+    make the move for input
+  else
+    ask for input again until you get a valid input
   end

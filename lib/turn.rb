45efec29c4f4b99495df51dd1932efaedc0e64c2
Devin Cooper <dcoope12@asu.edu>
2020-09-03 22:02:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,3 +1,19 @@
 def display_board(board)
 puts board
 end
+
+def position_taken?(board, index)
+if board[index] == " " || board[index] == "" || board[index] == nil
+return false
+elsif board[index] == "X" || board[index] == "O"
+return true
+end
+end
+
+def valid_move?(board, index)
+if index.between?(0,8) == true && position_taken? == false
+  return true 
+else 
+  return false
+end
+end

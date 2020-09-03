0ec85656cc4dfe9b8f17063fc0a8216400e4eb62
Devin Cooper <dcoope12@asu.edu>
2020-09-03 22:23:17 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,4 @@
-  def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  def display_board(board)
 
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
@@ -7,27 +7,39 @@
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
   end
+#convert user's placeon board to index integer using input_to_index
 
-def position_taken?(board, index)
-if board[index] == " " || board[index] == "" || board[index] == nil
-return false
-elsif board[index] == "X" || board[index] == "O"
-return true
-end
-end
+def valid_move?(board,index)
+  def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+  return false
+else
+  return true
+  end
+  end
 
-def valid_move?(board, index)
-if index.between?(0,8) == true && position_taken? == false
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def on_board?(number)
+if number.between?(0,8) == true
   return true
 else
   return false
 end
 end
 
-def move(board, index, token = "X")
- board[index] = token
+if  (position_taken?(board,index)) == false && (on_board?(index) == true)
+  return true
+else
+  return false
+end
 end
 
-def turn(board)
-  puts "Please enter 1-9:"
+def move(board, index, character = "X")
+  board[index] = character
+  return board
 end
+
+#Asks user for a number, check if it is valid, and if it's not, recursively continues to ask for a number
+def turn
+
+end 
